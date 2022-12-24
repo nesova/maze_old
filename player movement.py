43d9cf6a5ddb405f5ad9a5b08de7bdec50be814f
nesova <119742990+nesova@users.noreply.github.com>
2022-12-24 18:33:47 +0300

    Update player movement.py
    door + keys

--- player movement.py
+++ player movement.py
@@ -6,11 +6,11 @@ MAP = [
     '# ### #####',
     '# # #     #',
     '# # # ### #',
-    '#K# # # # #',
+    '#K# # #K# #',
     '### # # # #',
     '#   # #   #',
     '# # # # # #',
-    '# #   # # #',
+    '# #   # #K#',
     '#X#########',
 ]
 BLOCK_SIDE = 50
@@ -63,6 +63,41 @@ class Player:
             self.x += self.step
 
 
+class Exit:
+    def __init__(self):
+        self.open_key = 0
+        self.pos = None
+        self.door_textures = []
+        for i in range(3):
+            self.door_textures.append(pygame.image.load(f'images/image_door{i}.png'))
+
+    def get_coord(self, x, y):
+        self.pos = (x, y)
+
+    def return_exit_coord(self):
+        return self.pos
+
+    def draw(self, texture_n):
+        screen.blit(self.door_textures[texture_n], self.pos)
+
+
+class Keys:
+    def __init__(self):
+        self.keys = {}
+        self.texture = pygame.image.load(f'images/key.png')
+
+    def get_coord(self, x, y):
+        self.keys[(x, y)] = "not taken"
+
+    def update_is_taken(self, pos, is_taken):
+        self.keys[pos] = is_taken
+
+    def draw(self):
+        for key in self.keys:
+            if self.keys[key] == "not taken":
+                screen.blit(self.texture, key)
+
+
 class Direction:
     NONE = 0
     UP = 1
@@ -75,17 +110,31 @@ class Maze:
     def __init__(self):
         self.wall = Wall()
         self.player = Player()
+        self.keys = Keys()
+        self.exit = Exit()
         for i in range(HEIGHT):
             for j in range(WIDTH):
                 if MAP[i][j] == '#':
                     self.wall.get_coord(j * BLOCK_SIDE, i * BLOCK_SIDE)
                 elif MAP[i][j] == "@":
                     self.player.get_pos(j * BLOCK_SIDE, i * BLOCK_SIDE)
+                elif MAP[i][j] == 'X':
+                    self.exit.get_coord(j * BLOCK_SIDE, i * BLOCK_SIDE)
+                elif MAP[i][j] == 'K':
+                    self.keys.get_coord(j * BLOCK_SIDE, i * BLOCK_SIDE)
         self.pos = self.wall.return_walls_coords()
 
     def draw(self):
         self.wall.draw()
         self.player.draw()
+        self.keys.draw()
+        self.exit.draw(0)
+
+    def win(self):
+        if self.get_player_cell() == (self.exit.x, self.exit.y) and self.exit.open_key == len(self.key):
+            for i in range(4):
+                self.exit.draw(i)
+            return True
 
     def set_player_direction(self, direction):
         if self.player_can_move(direction):
@@ -118,8 +167,8 @@ class Maze:
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    font = pygame.font.SysFont('arial', 60)
     maze = Maze()
+    font = pygame.font.SysFont('arial', 60)
 
     running = True
 
